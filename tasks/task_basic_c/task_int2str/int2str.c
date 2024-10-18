d2cbc88a23f177f2e1c79fb591aa46d282561b98
Dmitriy <80101092+Vokaksab@users.noreply.github.com>
2024-10-18 23:11:28 +0000

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,38 @@
-#include <assert.h>
+#include <stdio.h>
 #include "int2str.h"
-#include "stdio.h"
 
-char* int2str(int number) {
-    char* str = "0";
-    return str;
+char* int2str(int value) {
+    static char buffer[12]; 
+    char* ptr = buffer;     
+    long long num = (long long)value; 
+    int isNegative = 0;
+
+    if (num == 0) {
+        return "0";
+    }
+    
+    if (num < 0) {
+        isNegative = 1;
+        num = -num;
+    }
+
+    while (num > 0) {
+        *ptr++ = (num % 10) + '0';
+        num /= 10;
+    }
+
+    if (isNegative) {
+        *ptr++ = '-';
+    }
+
+    char* start = buffer;
+    char* end = ptr - 1;
+
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+
+    return buffer;
 }
